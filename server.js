ec47c0f4be2de393eadff681da914e543e0964fc
mgrinko <glukym@gmail.com>
2016-10-31 19:44:33 +0200

    Small refactoring

---
+++ server.js
@@ -0,0 +1,19 @@
+var http = require('http');
+var static = require('node-static');
+var file = new static.Server('.', {
+  cache: 0
+});
+
+function accept(req, res) {
+  if (req.url.indexOf('/data/') === 0) {
+    setTimeout(function() {
+      file.serve(req, res);
+    }, 1000);
+  } else {
+    file.serve(req, res);
+  }
+}
+
+http.createServer(accept).listen(8080);
+
+console.log('Server running on port 8080');
